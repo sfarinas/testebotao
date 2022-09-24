3f49b6fb400bd37103cd3dda895338644c235320
sfarinas <sfarinas@gmail.com>
2022-09-24 20:27:15 -0300

    update teste com link do Canal add ramdon numero. add afc

--- app/src/main/java/com/farina/testebotao/MainActivity.java
+++ app/src/main/java/com/farina/testebotao/MainActivity.java
@@ -1,10 +1,14 @@
 package com.farina.testebotao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewTreeViewModelKt;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
     public void alterarTexto(View view){
         TextView texto = findViewById(R.id.acTextView);
         texto.setText("https://www.youtube.com/c/AlexandreCabo");
+        //texto.setOnClickListener();
+    }
+
+    public void alterarTexto2(View view){
+        TextView texto2 = findViewById(R.id.afcTextView2);
+        texto2.setText("http://autofacilconsultoria.com");
+    }
+
+    public void jogar(View view){
+        TextView jogoTexto = findViewById(R.id.numeroSorteText);
+        int numero = new Random().nextInt(11);
+        jogoTexto.setText("Numero selecionado é : " + numero);
     }
 
 }
